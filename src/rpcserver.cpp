53549cb7c3540c6ce683f688e9507b385a671894
Polytimos <33665698+polytimos@users.noreply.github.com>
2019-03-22 10:51:07 +0500

    Update rpcserver.cpp

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -202,10 +202,10 @@ Value stop(const Array& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "Stop Polytimos server.");
+            "Stop Polytimos daemon.");
     // Shutdown will take long enough that the response should get back
     StartShutdown();
-    return "Polytimos server stopping";
+    return "Polytimos daemon stopping";
 }
 
 
